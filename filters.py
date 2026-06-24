2ba5a79de66ff4058848afff08cdf6c08cd0ca76
Vladimir <ivanov33vladimir@gmail.com>
2026-06-24 20:28:25 +0300

    I'am no body i'am no body.........

--- filters.py
+++ filters.py
@@ -1,3 +1,6 @@
+from email.utils import parsedate_to_datetime
+from datetime import datetime, timezone, timedelta
+
 def filter_by_keyword(news, keyword):
     if not keyword:
         return news
@@ -5,9 +8,6 @@ def filter_by_keyword(news, keyword):
         item for item in news
         if keyword.lower() in item["title"].lower()
     ]
-
-from email.utils import parsedate_to_datetime
-from datetime import datetime, timezone, timedelta
 
 def filter_by_days(news, days):
     if not days:
